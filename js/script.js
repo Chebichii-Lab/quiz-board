4719cf89ace922c36f693a38ce143c5de8c872b7
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-05-03 10:08:05 +0300

    added changes in the file

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ var result2 = 2;
 var result3 = 3;
 var result4 = 4;
 var result5 = 5;
+var result6 = 6;
+var result7 = 7;
+var result8 = 8;
+var result9 = 9;
+var result10 = 10;
 
 function score () {
   return "you have scored" + total;
@@ -14,6 +19,11 @@ $(document).ready(function() {
     var question3 = $("input[name='question3']:checked").val();
     var question4 = $("input[name='question4']:checked").val();
     var question5 = $("input[name='question5']:checked").val();
+    var question6 = $("input[name='question6']:checked").val();
+    var question7 = $("input[name='question7']:checked").val();
+    var question8 = $("input[name='question8']:checked").val();
+    var question9 = $("input[name='question9']:checked").val();
+    var question10 = $("input[name='question10']:checked").val();
     console.log(question1);
     console.log(question2);
     console.log(question3);
