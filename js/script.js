5ac680abfba40d3a0251606a4b68f13a0339655d
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-05-05 12:04:16 +0300

    added changes in the file

--- js/script.js
+++ js/script.js
@@ -1,94 +1,55 @@
-var result1 = 1;
-var result2 = 2;
-var result3 = 3;
-var result4 = 4;
-var result5 = 5;
-var result6 = 6;
-var result7 = 7;
-var result8 = 8;
-var result9 = 9;
-var result10 = 10;
-
-function score () {
-  return "you have scored" + total;
+// BUSINESS LOGIC
+var sum = 0;
+function scoreCalculator(userValue) {
+  var names = ["js", "year", "framework","function","dom","link"];
+  names.forEach(function(name) {
+    var userValue = $("input:radio[name=" + name + "]:checked").val();
+    if(userValue === "10") {
+      sum+=10;
+    }
+  });
 }
-$(document).ready(function() {
-  $("#button1").click(function() {
-    var question1 = $("input[name='question1']:checked").val();
-    var question2 = $("input[name='question2']:checked").val();
-    var question3 = $("input[name='question3']:checked").val();
-    var question4 = $("input[name='question4']:checked").val();
-    var question5 = $("input[name='question5']:checked").val();
-    var question6 = $("input[name='question6']:checked").val();
-    var question7 = $("input[name='question7']:checked").val();
-    var question8 = $("input[name='question8']:checked").val();
-    var question9 = $("input[name='question9']:checked").val();
-    var question10 = $("input[name='question10']:checked").val();
-    console.log(question1);
-    console.log(question2);
-    console.log(question3);
-    console.log(question4);
-    console.log(question5);
-    console.log(question6);
-    console.log(question7);
-    console.log(question8);
-    console.log(question9);
-    console.log(question10);
 
-    if (question1 === "right") {
-        result1 = 20;
-      }else {
-        result1 = 0;
-      }
-      if (question2 ==="right") {
-        result2 = 20;
-      } else {
-        result2 = 0;
-      }
-      if (question3 === "right") {
-        result3 = 20;
-      } else {
-        result3 = 0;
-      }
-      if (question4 === "right") {
-        result4 = 20;
-      } else {
-        result4 = 0;
-      }
-      if (question5 === "right") {
-        result5 = 20;
-      }else {
-        result5= 0;
-      }
-      if (question6 === "right") {
-          result6 = 20;
-      }else {
-          result6= 0;
-      }
-      if (question7 === "right") {
-          result7 = 20;
-      }else {
-          result6= 0;
-      }
-      if (question8 === "right") {
-          result8 = 20;
-      }else {
-          result8= 0;
-      }
-      if (question9 === "right") {
-          result9 = 20;
-      }else {
-          result9= 0;
-      }
-      if (question10 === "right") {
-          result10 = 20;
-      }else {
-          result10= 0;
-      }
-      var score = (result1 + result2 + result3 + result4 + result5 + result6 + result7 + result8 + result9 + result10);
-      console.log(score);
-  
-      $("#result").text("Congratulations! You have gotten " + score + " points.");
-      
-    })
+
+// USER INTERFACE LOGIC
+$(function() {
+  $("#start-btn").click(function(event) {
+    event.preventDefault();
+    var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+    if(userFirstName.length === 0 || userSecondName.length === 0) {
+      $("#no-name").show();
+      $("#firstName").addClass("enter-name");
+      $("#secondName").addClass("enter-name");
+    } else {
+      $("#intro").hide();
+      $("#form1").show();
+    }
   })
+  $("#arrow-right").click(function() {
+    $("#form1").hide();
+    $("#form2").show();
+  })
+  $("#arrow-left").click(function() {
+    $("#form1").show();
+    $("#form2").hide();
+  })
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    $("#score").show();
+    $("#form2").hide();
+    scoreCalculator();
+    var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+    if(sum <= 30) {
+      $(".name-display").text(`Nice try ${userFirstName} ${userSecondName},`);
+    } else {
+      $(".name-display").text(`Congratulations ${userFirstName} ${userSecondName},`);
+    }
+    $(".scoreDisplay").text(sum + "/60");
+  })
+  $("#retake").click(function(event) {
+    event.preventDefault();
+    location.reload();
+  })
+})
